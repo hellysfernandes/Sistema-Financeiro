e1e881775131a11c021e3c0d421665743fb5fe9c
Hellys Fernandes <hellysfernandesrotimei@gmail.com>
2026-09-22 11:01:15 -0300

    padronisação do schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,7 @@
 CREATE TABLE transacoes (
 id INTEGER PRIMARY KEY,
--- id_carteira INTEGER NOT NULL,
 descricao TEXT NOT NULL,
 valor REAL NOT NULL,
 data TEXT NOT NULL,
 tipo TEXT NOT NULL
 );
-
--- Provavel tabela de carteira ou conta de pagamento
-CREATE TABLE carteira ou conta (
-id INTEGER PRIMARY KEY,
-nome TEXT NOT NULL.
-tipo TEXT NOT NULL,
-valor DOUBLE -- ver questao de permanecia de daods ?
-);
-
--- talvez tabela que guarda a informação da semana ou mes
